29fc462a1e6d049a36e1860a8037a37d7b4a29cc
student <student@C246U57>
2020-11-12 16:50:54 +0300

    alpha 0000.1

---
+++ Client1_0/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client1_0
+{
+  /// <summary>
+  /// Логика взаимодействия для MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+      TextBox txtBox = new TextBox();
+        txtBox.Width = 500;
+        txtBox.Height = 20;
+      txtBox.Text = Mess.Text;
+      txtBox.IsEnabled = false;
+      Thickness m = txtBox.Margin;
+      m.Bottom = 10;
+      txtBox.Margin = m;
+      MyPanel.Children.Add(txtBox);
+      Mess.Text = "";
+    }
+
+    private void Mess_KeyDown(object sender, KeyEventArgs e)
+    {
+      if (e.Key == Key.Enter)
+      {
+        TextBox txtBox = new TextBox();
+        txtBox.Width = 500;
+        txtBox.Height = 20;
+        txtBox.Text = Mess.Text;
+        txtBox.IsEnabled = false;
+        Thickness m = txtBox.Margin;
+        m.Bottom = 10;
+        txtBox.Margin = m;
+        MyPanel.Children.Add(txtBox);
+        Mess.Text = "";
+      }
+    }
+  }
+}
